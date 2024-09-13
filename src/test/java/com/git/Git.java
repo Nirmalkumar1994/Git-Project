d3a7e7dc513615470086ce8fb8115531f1c8de11
Nirmalkumar1994 <nirmalkumar.ekr@gmail.com>
2024-09-13 12:49:27 +0530

    Git

---
+++ src/test/java/com/git/Git.java
@@ -0,0 +1,21 @@
+package com.git;
+
+public class Git {
+
+		
+		public void name() {
+			
+			System.out.println(" Nirmal ");
+		}
+
+		public void id() {
+			System.out.println(" id 1232 ");
+		}
+		
+		public void address() {
+			System.out.println(" Chennai ");
+		}
+	}
+
+
+
